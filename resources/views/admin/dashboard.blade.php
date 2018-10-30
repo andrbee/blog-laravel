42da3fb669266dbdeed2ced736613d0718eccc22
andrbee <andrbee@gmail.com>
2018-10-30 23:15:44 +0300

    Created Dashboard

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,17 @@
+@extends('admin.layouts.app_admin')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-3">Категорий 0</div>
+            <div class="col-sm-3">Материалов 0</div>
+            <div class="col-sm-3">Посетителей 0</div>
+            <div class="col-sm-3">Сегодня 0</div>
+        </div>
+
+        <div class="row">
+            <div class="col-sm-6">Создать категорию</div>
+            <div class="col-sm-6">Создать материал</div>
+        </div>
+    </div>
+@endsection('content')
